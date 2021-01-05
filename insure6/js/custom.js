80bcb4e034cd406c99dfc8b68429f35b54be1133
Dharambir Singh <58848969+dharambirx@users.noreply.github.com>
2021-01-06 01:59:10 +0530

    insure6

---
+++ insure6/js/custom.js
@@ -0,0 +1,7 @@
+//[Custom Javascript]
+
+//Project:	Insurance Agency - Onepage Html Responsive Template
+//Version:	1.1
+//Primary use:	Insurance Agency - Onepage Html Responsive Template 
+
+//add your script here
